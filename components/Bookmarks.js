b1155b58748d381550a1fe028a82d04f9e54385b
Charles Fontaine <charles@zoomalia.com>
2026-04-15 16:18:07 +0200

    feat(Bookmarks): display articles bookmarked with the reuse of Article component

--- components/Bookmarks.js
+++ components/Bookmarks.js
@@ -1,20 +1,29 @@
-import Head from 'next/head';
-import styles from '../styles/Bookmarks.module.css';
-
+import Head from "next/head";
+import { useSelector } from "react-redux";
+import styles from "../styles/Bookmarks.module.css";
+import Article from "./Article";
 
 function Bookmarks() {
-	return (
-		<div>
-			<Head>
-				<title>Morning News - Bookmarks</title>
-			</Head>
-			<div className={styles.container}>
-				<h2>Bookmarks</h2>
-				<p>No bookmark</p>
-			</div>
-		</div>
+  // state fait référence au store
+  const bookmarks = useSelector((state) => state.bookmarks.value);
+  let articles = <p>No bookmark</p>;
 
-	);
+  if (bookmarks.length) {
+    articles = bookmarks.map((data, i) => {
+      return <Article key={i} {...data} isBookmarked />;
+    });
+  }
+  return (
+    <div>
+      <Head>
+        <title>Morning News - Bookmarks</title>
+      </Head>
+      <div className={styles.container}>
+        <h2>Bookmarks</h2>
+        {articles}
+      </div>
+    </div>
+  );
 }
 
 export default Bookmarks;
